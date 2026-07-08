7582292b865288c215e173c52cca4016ce4a74a1
PradoRuhan <ruhan-prado@hotmail.com>
2026-07-07 23:09:35 -0300

    fix: corrige tag de link no dropdown do navbar

--- src/app/Shared/components/navbar/navbar.ts
+++ src/app/Shared/components/navbar/navbar.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  constructor(
+    public authService: AuthService,
+    private router: Router
+  ) {}
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
