059d359b5a0aa3986664297483ae6090d1dd33cb
PradoRuhan <ruhan-prado@hotmail.com>
2026-07-07 22:10:43 -0300

    Feat: adiciona guards de autenticação e configuração de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
+import { roleGuard } from './core/guards/role.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/landing/landing').then((m) => m.Landing),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login').then((m) => m.Login),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/auth/register/register').then((m) => m.Register),
+  },
+  {
+    path: 'dashboard/producer',
+    loadComponent: () =>
+      import('./features/producer-dashboard/producer-dashboard').then(
+        (m) => m.ProducerDashboard
+      ),
+    canActivate: [roleGuard(['producer'])],
+  },
+  {
+    path: 'dashboard/admin',
+    loadComponent: () =>
+      import('./features/admin-dashboard/admin-dashboard').then(
+        (m) => m.AdminDashboard
+      ),
+    canActivate: [roleGuard(['admin'])],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
